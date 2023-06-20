718cec846a5600a27204e49f15408cc3c0dd4dfb
AladinJmila <jmila.aladin@gmail.com>
2023-06-20 08:49:59 +0100

    Rename first app to diaries

--- diraies/apps.py
+++ diraies/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EmotionsConfig(AppConfig):
+class DiariesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'emotions'
+    name = 'diaries'
