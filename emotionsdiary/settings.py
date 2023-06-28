0ef56d6c3e8aa757118fc846518c7132f00a411c
AladinJmila <jmila.aladin@gmail.com>
2023-06-28 08:57:56 +0100

    Add cors middleware

--- emotionsdiary/settings.py
+++ emotionsdiary/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'debug_toolbar',
     'diaries',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
 
 INTERNAL_IPS = [
     '127.0.0.1'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5173'
 ]
 
 ROOT_URLCONF = 'emotionsdiary.urls'
