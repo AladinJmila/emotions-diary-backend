ec2edd5be67b213d196d1ef15d30a57ca38c04d9
AladinJmila <jmila.aladin@gmail.com>
2023-06-28 07:33:47 +0100

    Add test DELETE

--- diaries/tests.py
+++ diaries/tests.py
@@ -20,16 +20,12 @@ class EmotionalStateAPITestCase(TestCase):
     apiEndpoint = '/api/emotional-states'
 
     def test_create_emotional_state(self):
-        # Simulate a POST request to the API endpoint
         response = self.client.post(f'{self.apiEndpoint}/', data=self.payload)
 
-        # Assert the expected response status code
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        # Assert taht the emotional state was created in the database
         self.assertEqual(EmotionalState.objects.count(), 1)
 
-        # Assert the values of the created emotional state
         emotional_state = EmotionalState.objects.first()
         self.assertEqual(emotional_state.name, self.payload['name'])
         self.assertEqual(emotional_state.date, self.payload['date'])
@@ -40,18 +36,14 @@ class EmotionalStateAPITestCase(TestCase):
                          self.payload['coping_mechanisms'])
 
     def test_retrieve_emotional_state(self):
-        # Create an instance of EmotionalState
         emotional_state = EmotionalState.objects.create(**self.payload)
 
-        # Simulate a GET request to the API endpoint
         response = self.client.get(
             f'{self.apiEndpoint}/{emotional_state.id}/', follow=True)
 
-        # Asssert expected response status code
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_emotional_state(self):
-        # Create and instance of EmotionalState
         emotional_state = EmotionalState.objects.create(**self.payload)
 
         payload = {
@@ -60,19 +52,23 @@ class EmotionalStateAPITestCase(TestCase):
             'triggers': 'Music, Nature'
         }
 
-        # Simulate a PATCH request to the API endpoint
         response = self.client.patch(
             f'{self.apiEndpoint}/{emotional_state.id}/', data=payload, content_type='application/json')
 
-        # Assert expected response status code
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # Retrieve the updated emotional state object
         updated_emotional_state = EmotionalState.objects.get(
             id=emotional_state.id)
 
-        # Check that the fields were updated correctly
         self.assertEqual(updated_emotional_state.name, payload['name'])
         self.assertEqual(updated_emotional_state.intensity,
                          payload['intensity'])
         self.assertEqual(updated_emotional_state.triggers, payload['triggers'])
+
+    def test_delete_emotional_state(self):
+        emotional_state = EmotionalState.objects.create(**self.payload)
+
+        response = self.client.delete(
+            f'{self.apiEndpoint}/{emotional_state.id}/')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
