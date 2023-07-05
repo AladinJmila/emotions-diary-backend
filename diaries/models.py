43fc9a803014789489a3aa95891db0357e3e0eff
AladinJmila <jmila.aladin@gmail.com>
2023-07-05 09:28:08 +0100

    Successfully save data from client

--- diaries/models.py
+++ diaries/models.py
@@ -3,11 +3,13 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class EmotionalState(models.Model):
+    date = models.DateField(auto_now_add=True)
     name = models.CharField(max_length=255)
-    date = models.DateField()
+    description = models.TextField()
     energy = models.FloatField(
         validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
     intensity = models.FloatField(
         validators=[MinValueValidator(0.0), MaxValueValidator(1.0)])
     triggers = models.TextField()
     coping_mechanisms = models.TextField()
+    tags = models.TextField()
